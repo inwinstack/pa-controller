774280e826bc442ff47a8653ed34a3e2ccf6d23d
Kyle Bai <k2r2.bai@gmail.com>
2019-08-06 23:22:53 +0800

    Add support for enabling high availability (#44)
    * Add support for enabling high availability
    
    * Fix flag description
    
    * Update HA inspector for verifying suspend state
    
    * Update go.sum

--- go.mod
+++ go.mod
@@ -5,11 +5,10 @@ go 1.12
 require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/inwinstack/blended v0.7.0
-	github.com/inwinstack/pango v0.3.1
+	github.com/inwinstack/pango v0.4.1
 	github.com/spf13/pflag v1.0.1
 	github.com/stretchr/testify v1.2.2
 	github.com/thoas/go-funk v0.4.0
-	k8s.io/api v0.0.0-20190620084959-7cf5895f2711
 	k8s.io/apiextensions-apiserver v0.0.0-20190620085554-14e95df34f1f
 	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
 	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
---
+++ pkg/ha/ha_test.go
@@ -0,0 +1,51 @@
+/*
+Copyright © 2018 inwinSTACK Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ha
+
+import (
+	"context"
+	"testing"
+
+	"github.com/inwinstack/pango/testdata"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHAInspector(t *testing.T) {
+	ch := make(chan bool, 1)
+	ctx, cancel := context.WithCancel(context.Background())
+	mc := &testdata.MockClient{}
+
+	callbacks := &Callbacks{
+		OnActive: func() {
+			ch <- true
+		},
+		OnPassive: func() {
+			ch <- false
+		},
+		OnFail: func(err error) {
+			ch <- false
+			t.Log(err)
+		},
+	}
+
+	inspector := NewInspector(mc, 30, callbacks)
+	assert.Nil(t, inspector.Run(ctx))
+
+	state := <-ch
+	assert.Equal(t, true, state)
+	cancel()
+}
